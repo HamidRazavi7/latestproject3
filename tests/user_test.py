c2c00cf7138fc398a3a74f2d14f4d5a6437eb124
Hamid Razavi <razavi.hamid85@gmail.com>
2022-04-27 22:37:07 +0800

    create a database record for title, artist, year, and genre.

--- tests/user_test.py
+++ tests/user_test.py
@@ -25,18 +25,24 @@ def test_adding_user(application, add_user):
         # asserting that the user retrieved is correct
         assert user.email == 'keith@webizly.com'
         # this is how you get a related record ready for insert
-        user.songs = [Song("test", "Mariah Karey"), Song("test2", "Justin Bieber")]
+        user.songs = [
+            Song("all i want for christmas is you", "Mariah Karey", "Pop", 2015),
+            Song("yummy", "Justin Bieber", "Hip Hop", 2019)
+        ]
         # commit is what saves the songs
         db.session.commit()
         assert db.session.query(Song).count() == 2
-        song1 = Song.query.filter_by(title='test').first()
-        assert song1.title == "test"
+        song1 = Song.query.filter_by(title='all i want for christmas is you').first()
+        assert song1.title == "all i want for christmas is you"
+        assert song1.genre == "Pop"
         # changing the title of the song
         song1.title = "SuperSongTitle"
         # saving the new title of the song
         db.session.commit()
         song2 = Song.query.filter_by(title='SuperSongTitle').first()
         assert song2.title == "SuperSongTitle"
+        assert song2.year == 2015
+        assert song2.artist == "Mariah Karey"
         # checking cascade delete
         db.session.delete(user)
         assert db.session.query(User).count() == 0
