6ddd85bbdd69bece03350d1d9ccb7baa91cea2cb
Hamid Razavi <razavi.hamid85@gmail.com>
2022-04-29 05:20:26 +0800

    updated the database_test.py

--- tests/database_test.py
+++ tests/database_test.py
@@ -2,7 +2,11 @@ def test_upload_csvfile(client):
     file = "./uploads/music_copy.csv"
     data = { 'file': (open(file, 'rb'), file) }
 
-    # I should copy the logging code here once login test works.
+    # once the login test works I copy that code here for logging in
     res = client.get("/login")
     assert res.status_code == 200
-    # response = client.post('/songs/upload', data=data)
+    response = client.post('/songs/upload', data=data)
+
+    # uncomment this once we have the login test since I should be able to get past the CSRF error.
+    # assert response.status_code == 201
+
