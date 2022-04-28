86edd2216c50bd75d6476a612122845908194cfb
Hamid Razavi <razavi.hamid85@gmail.com>
2022-04-28 16:06:13 +0800

    reformatted the dunder file.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,38 +5,37 @@ import pytest
 from app import create_app, User
 from app.db import db
 
-#this is a good tutorial I used to fix this code to do datbase testing.
-#https://xvrdm.github.io/2017/07/03/testing-flask-sqlalchemy-database-with-pytest/
+
+# this is a good tutorial I used to fix this code to do datbase testing.
+# https://xvrdm.github.io/2017/07/03/testing-flask-sqlalchemy-database-with-pytest/
 
 @pytest.fixture()
 def application():
     """This makes the app"""
     application = create_app()
     application.config.update(
-        #will save to the database file you can view
-        ENV='development',
-        #will save to memory / you can't see but runs fast
-        #ENV='testing',
+        # will save to the database file you can view
+        ENV='testing',
+        # will save to memory / you can't see but runs fast
+        # ENV='testing',
 
     )
     with application.app_context():
         db.create_all()
-        #creates tables
+        # creates tables
         yield application
         db.session.remove()
-        #removes tables when test is over, drops the database tables after the test runs
+        # removes tables when test is over, drops the database tables after the test runs
         db.drop_all()
 
 
 @pytest.fixture()
 def add_user(application):
     with application.app_context():
-        #new record
+        # new record
         user = User('keith@webizly.com', 'testtest')
         db.session.add(user)
         db.session.commit()
-
-
 
 
 @pytest.fixture()
